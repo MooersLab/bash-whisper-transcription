77059675377db114cf2a533d339cc32169100804
Blaine Mooers <bmooers1@gmail.com>
2024-04-04 12:54:31 -0500

    Update replacem.py
    Clean up

--- replacem.py
+++ replacem.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 from contractions import replaceContractions
-from punctuations import replacePunctuations
-from domain-specific import replaceBiochemistry, replaceVirology, replaceProgramming
-from xtaleq import replaceXtalEq
 from pathlib import Path
 import sys
 
@@ -11,7 +8,7 @@ import sys
 This is the master script for applying imported text replacement libraries to transcribed text from whisper.
 We set up this project to be modular because different people will want different combinations of text replacements.
 This also eases the development of new libraries by users.
-The external files or modules that we have defined reside in the project directory.
+The external files or modules we defined reside in the project directory.
 
 In principle, we could install this project on PyPi and then people could pip install it into their local Python library.
 However, that seems to be overkill in this case because this is a trivial project.
@@ -20,14 +17,11 @@ However, that seems to be overkill in this case because this is a trivial projec
 txt = Path(sys.argv[1]).read_text()
 my_file = open(sys.argv[1] + "corrected.txt", "w")
 
-"""import the text replacement functions
+"""Import the text replacement functions.
 Note that you have to define a new variable that
 is mapped to the returned processed text.
 """
 
-retxt = replaceContractions(bltxt)
-ptxt = replacePunctuations(retxt)
-eqtxt = replaceXtalEq(latxt)
-#protxt = replaceProjects(eqtxt)
-my_file.write(eqtxt)
+retxt = replaceContractions(txt)
+my_file.write(retxt)
 my_file.close()

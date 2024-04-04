1ca89d9023159d7295241fb17045000204ac4bd7
Blaine Mooers <bmooers1@gmail.com>
2024-04-04 12:45:07 -0500

    Update and rename biochemistry.py to domain-specific.py

--- domain-specific.py
+++ domain-specific.py
@@ -6,9 +6,6 @@ def replaceBiochemistry(txt):
     txt = re.sub("expand LLPS", "liquid-liquid phase separation", txt, flags=re.IGNORECASE)
     txt = re.sub("expand GEO", "Gene Expression Omnibus", txt, flags=re.IGNORECASE)
     txt = re.sub("expand AD", "Alzheimer's Disease", txt, flags=re.IGNORECASE)
-    
-
-
     return txt
     
 def replaceVirology(txt):
@@ -17,21 +14,9 @@ def replaceVirology(txt):
         txt = re.sub("expand LLPS", "liquid-liquid phase separation", txt, flags=re.IGNORECASE)
         txt = re.sub("expand GEO", "Gene Expression Omnibus", txt, flags=re.IGNORECASE)
         txt = re.sub("expand AD", "Alzheimer's Disease", txt, flags=re.IGNORECASE)
-    
     return txt
     
-    
-    
-    
+        
 def replaceProgramming(txt):
     txt = re.sub("expand CFI", "C-Fortran Interoperability", txt, flags=re.IGNORECASE)
- 
- 
     return txt
-    
-    
-    
-def replaceProgramming(txt):    
-    
-    txt = re.sub("expand MCDM", "multiple criteria decision-making", txt, flags=re.IGNORECASE)
- 

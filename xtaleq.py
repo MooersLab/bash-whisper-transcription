ad40e0842a8135b618e968be98ad5455cf17d1f7
Blaine Mooers <bmooers1@gmail.com>
2024-04-04 12:35:45 -0500

    Update xtaleq.py

--- xtaleq.py
+++ xtaleq.py
@@ -1,6 +1,6 @@
 import re
 
 def replaceXtalEq(txt):
-    structureFactorEquation=r"""$$\\mathbf{F}(\\mathbf{S})=\\sum_{j=1}^n f_j \\exp \\left[2 \\pi i \\mathbf{r}_j \\cdot \\mathbf{S}\\right]$$"""
+    structure factor equation=r"""$$\\mathbf{F}(\\mathbf{S})=\\sum_{j=1}^n f_j \\exp \\left[2 \\pi i \\mathbf{r}_j \\cdot \\mathbf{S}\\right]$$"""
     txt = re.sub("insert the structure factor equation", structureFactorEquation, txt, flags=re.IGNORECASE)
     return txt
